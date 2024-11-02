b2013016acc8c4d7d5e760072fcf97753b0fae63
GETRDP2024 <154799632+GETRDP2024@users.noreply.github.com>
2024-11-03 03:26:50 +0530

    check form submission

--- assets/js/submit_form.js
+++ assets/js/submit_form.js
@@ -11,3 +11,24 @@ function showThankYouMessage() {
     document.getElementById('registrationForm').reset(); // Reset the form
     document.getElementById('thankYouMessage').style.display = 'block'; // Show thank you message
 }
+
+
+
+ // Initialize EmailJS
+ 
+
+// Submit form event listener
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    // Send form data using EmailJS
+    emailjs.sendForm("service_g0bs1xt", "template_f82vp4e", this)
+        .then(function() {
+            // Show thank you message
+            document.getElementById("thankYouMessage").style.display = "block";
+            // Clear the form
+            document.getElementById("contactForm").reset();
+        }, function(error) {
+            console.error("Failed to send email:", error);
+        });
+});
